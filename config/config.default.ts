a641d577005c25bd04df60a4e2735eeab2e27fa8
alpha <41134701+Alpha-CL@users.noreply.github.com>
2022-09-29 14:53:52 +0800

    feat(install plugins):

--- config/config.default.ts
+++ config/config.default.ts
@@ -2,6 +2,80 @@ import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
 
 export default (appInfo: EggAppInfo) => {
   const config = {} as PowerPartial<EggAppConfig>;
+	
+	/** custom config zone start **/
+	
+	config.redis = {
+		client: {
+			host: '47.109.23.107',
+			port: 6379,
+			password: 'Liang1992@12',
+			db: 0,
+		},
+		agent: true
+	};
+	
+	config.mysql = {
+		client: {
+			host: '47.109.23.107',
+			port: '3306',
+			user: 'root',
+			password: 'Liang1992@12',
+			database: 'dev',
+		},
+	};
+	
+	config.sequelize = {
+		dialect: 'mysql',
+		host: '47.109.23.107',
+		port: 3306,
+		username: 'root',
+		password: 'Liang1992@12',
+		database: 'dev',
+	};
+	
+	config.cors = {
+		// origin: '*',
+		credentials: true,
+		allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+	};
+	
+	config.security = {
+		domainWhiteList: [
+			'localhost:8080',
+		],
+		csrf: {
+			enable: false,
+		},
+	};
+	
+	config.cache = {
+		default: 'memory',
+		stores: {
+			memory: {
+				driver: 'memory',
+				max: 100,
+				ttl: 0,
+			},
+		},
+	};
+	
+	config.jwt = {
+		secret: "123456"
+	};
+	
+	config.passportLocal = {
+		// usernameField: 'username',
+		// passwordField: 'password',
+	};
+	
+	exports.validate = {
+		// convert: false,
+		// validateRoot: false,
+	},
+	
+	
+	/** custom config zone end **/
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
--- config/plugin.ts
+++ config/plugin.ts
@@ -6,6 +6,55 @@ const plugin: EggPlugin = {
   //   enable: true,
   //   package: 'egg-view-nunjucks',
   // },
+	
+	/** custom plugins zone start **/
+	
+	redis: {
+		enable: true,
+		package: 'egg-redis',
+	},
+	
+	mysql: {
+		enable: true,
+		package: 'egg-mysql',
+	},
+	
+	sequelize: {
+		enable: true,
+		package: 'egg-sequelize',
+	},
+	
+	cors: {
+		enable: true,
+		package: 'egg-cors',
+	},
+	
+	security: {
+		enable: true,
+		package: 'egg-security',
+	},
+	
+	cache: {
+		enable: true,
+		package: 'egg-cache',
+	},
+	
+	jwt: {
+		enable: true,
+		package: "egg-jwt"
+	},
+	
+	session: {
+		enable: true,
+		package: 'egg-session',
+	},
+	
+	validate: {
+		enable: true,
+		package: 'egg-validate',
+	},
+	
+	/** custom plugins zone end **/
 };
 
 export default plugin;
